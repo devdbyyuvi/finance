b46857af3588999bca6a8fff72c4e2c1baf5e97f
Yuvraaj Narula <ynarula678@gmail.com>
2025-07-09 21:31:11 +0530

    gitignore + authROute

--- backend/routes/authRoute.js
+++ backend/routes/authRoute.js
@@ -1,4 +1,8 @@
 const express = require('express');
 const router = express.Router();
-
+const logger = require('../utils/logger');
+router.post('/register',(req,res)=>{
+    logger.info('Request body:', req.body);
+    return res.status(200).json(req.body)
+})
 module.exports = router;

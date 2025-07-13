a6026cfa9790297e3ae3c6f8931171ec54e3d725
Yuvraaj Narula <ynarula678@gmail.com>
2025-07-13 21:12:45 +0530

    backend pt II

--- backend/routes/budgetRoute.js
+++ backend/routes/budgetRoute.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const router = express.Router();
-const logger= require('../utils/logger');
+const logger = require('../utils/logger');
 const budgetController = require('../controllers/budget');
 const authMiddleware = require('../middleware/auth');
 const Budget = require('../models/budgetSchema');
 
-module.exports = router;
+router.post('/', authMiddleware, budgetController.createBudget);
+router.delete('/:id', authMiddleware, budgetController.deleteBudget);
+router.get('/:id', authMiddleware, budgetController.getBudget);
+router.put('/:id', authMiddleware, budgetController.updateBudget);
+router.get('/', authMiddleware, budgetController.getAllBudgets);
+router.post('/user', authMiddleware, budgetController.getUserBudgets);
+
+module.exports = router
--- backend/routes/transactionRoute.js
+++ backend/routes/transactionRoute.js
@@ -1,7 +1,14 @@
 const express = require('express');
 const router = express.Router();
-const logger= require('../utils/logger');
+const logger = require('../utils/logger');
 const transactionController = require('../controllers/transaction');
 const authMiddleware = require('../middleware/auth');
 const Transaction = require('../models/transactionSchema');
-module.exports = router;
+
+router.post('/', authMiddleware, transactionController.createTransaction);
+router.delete('/:id', authMiddleware, transactionController.deleteTransaction);
+router.get('/:id', authMiddleware, transactionController.getTransaction);
+router.put('/:id', authMiddleware, transactionController.updateTransaction);
+router.get('/', authMiddleware, transactionController.getAllTransactions);
+router.post('/user', authMiddleware, transactionController.getUserTransactions);
+module.exports = router
